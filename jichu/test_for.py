a5a0208f744acc794bfefed3e49600e3afb325b8
zhangkejin <zhangkejin@bytedance.com>
2020-06-29 22:47:58 +0800

    6月29
    Change-Id: I1f8ab80420f4ce0562afa6c6584008ed45707f42

--- jichu/test_for.py
+++ jichu/test_for.py
@@ -1,17 +1,58 @@
-#从1 至  10 计算偶数的和
+#! /usr/bin/env python
+# coding: utf8
+#
+# alist = [];
+# for i in range(1,11):
+#     if(i%2 ==0):
+#         alist.append(i*i)
+#
+# print(alist)
+# #列表推导式
+# blist = [i*i for i in range(1,11) if(i%2)==0]
+#
+# print(blist)
+#
+# #字典推导式
+# abb = ['a','b']
+# aaa = {i:0 for i in abb}
 
-alist = [];
-for i in range(1,11):
-    if(i%2 ==0):
-        alist.append(i*i)
 
-print(alist)
-#列表推导式
-blist = [i*i for i in range(1,11) if(i%2)==0]
+def fun():
+    list = []
+    strs = [108, 1873]
+    # 去除字符串两边的字符
+    # s = strs.strip("'[]'")
+    # print s
+    # # 以逗号拆分字符串，并逐一添加到list中
+    # a = int(s.split(',')[0])
+    # b = int(s.split(',')[1])
+    # list.append(a)
 
-print(blist)
+    for i in strs:
+        print(i)
 
-#字典推导式
-abb = ['a','b']
-aaa = {i:0 for i in abb}
 
+def fun1():
+    try:
+       a=12/0
+       print(a)
+    except Exception as e:
+       print(".....")
+    # print("!!!!!!!!/\",a)
+
+def fun3():
+    dict = {"name":"zhangkejin", "age":23}
+    print(dict["name"])
+    print "zhangkejin"
+
+
+
+def fun2():
+    a = 0b10
+    print("a:%s"%a)
+
+if __name__ == '__main__':
+    fun3()
+    fun2()
+    fun1()
+    print("asdf")
---
+++ python3/datatype.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python3
+"""
+不可变数据（3 个）：Number（数字）、String（字符串）、Tuple（元组）；
+可变数据（3 个）：List（列表）、Dictionary（字典）、Set（集合）。
+"""
+
+
+
+if __name__ == '__main__':
+    set()
+    set1 = {"zhangkejin", "wangwu", "lisi", "zhangsan", "zhangkejin"}
+    for str in set1:
+        print(str)
